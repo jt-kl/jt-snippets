8915b7ee9f610a3a670dac6f796a827d2852fb89
Jeremy Tan <jeremy.tan@astravirtus.com>
2022-09-17 16:52:47 +0800

    Added new methods

--- src/io_ops.py
+++ src/io_ops.py
@@ -3,60 +3,155 @@ import hashlib
 import pathlib
 import typing
 
+# region: Validators
+
+
+def validate_file_hash(
+    _hash: str,
+    path: pathlib.Path,
+    algorithm: str = "md5",
+) -> bool:
+    """
+    Helper method to validate a file hash
+
+    Args:
+        hash: Hash value to validate against
+        path: Source file path to generate hash
+        algorithM: Hashing algorithm to use. Example: "md5", "sha256"
+    """
+    return _hash == generate_file_hash(path, algorithm)
+
+
+def validate_path_exists(
+    path: pathlib.Path,
+):
+    """
+    Validates path exists
+
+    Args:
+        path: Path to be validated
+    """
+    if not path.exists():
+        raise Exception(f"Path doesn't exists.")
+
+
+def validate_is_directory(
+    path: pathlib.Path,
+):
+    """
+    Validates path is directory
+
+    Args:
+        path: Path to be validated
+    """
+    if not path.is_dir():
+        raise Exception(f"Path is not a directory.")
+
+
+def validate_is_file(
+    path: pathlib.Path,
+):
+    """
+    Validates path is file
+
+    Args:
+        path: Path to be validated
+    """
+    if not path.is_file():
+        raise Exception(f"Path is not a file.")
+
+
+def validate_file_size(
+    path: pathlib.Path,
+    lower_bound: int,
+    upper_bound: int,
+) -> bool:
+    """
+    Helper method to validate file size between specified ranges
+
+    Args:
+        path: Source file path to be validated
+        lower_bound: Acceptable lower bound range of file size in bytes
+        upper_bound: Acceptable upper bound range of file size in bytes
+    """
+    validate_is_file(path)
+
+    if not lower_bound >= upper_bound:
+        raise Exception(
+            f"Value of lower bound range cannot be greater "
+            f"than or equal to value of upper bound range"
+        )
+
+    return lower_bound < path.stat().st_size < upper_bound
+
+
+# endregion: Validators
+
 
 def create_directories(
-    directories: list[pathlib.Path],
+    paths: list[pathlib.Path],
+    **kwargs,
 ):
     """
     Helper method to create a collection of directories
 
     Args:
-        directories: Collection of directories to be created
+        paths: Collection of directory paths to be created
     """
-    [directory.mkdir(parents=True, exist_ok=True) for directory in directories]
+    [path.mkdir(parents=True, exist_ok=True, **kwargs) for path in paths]
 
 
 def delete_directory(
-    directory: pathlib.Path,
+    path: pathlib.Path,
     recursive: bool = False,
 ):
     """
-    Delete directory and its contents
+    Helper method to delete directory and its contents
 
     Args:
-        directory: Directory path to be deleted
-        recursive: Recursively deletes child directories
+        path: Directory path to be deleted
+        recursive: Recursively deletes child directories and its contents
     """
-    assert directory.exists(), f"Invalid directory path"
-    assert directory.is_dir(), f"Invalid directory path"
+    validate_path_exists(path)
+    validate_is_directory(path)
 
-    if not recursive:
-        directory.unlink()
+    # Deletes the only directory in the current directory
+    if not recursive and not len(list(path.iterdir())):
+        path.rmdir()
 
         return
 
-    for item in directory.iterdir():
-        if item and item.is_dir():
-            delete_directory(item, recursive)
-        else:
-            item.unlink(missing_ok=True)
+    # Deletes only files in the current directory
+    if not recursive and len(list(path.iterdir())):
+        [_path.unlink(missing_ok=True) for _path in path.iterdir() if _path.is_file()]
+
+        return
+
+    # Deletes both files and directory in the current directory recursivley
+    if recursive and len(list(path.iterdir())):
+        for _path in path.glob("**"):
+            if _path.is_file():
+                _path.unlink(missing_ok=True)
+
+            elif _path.is_dir():
+                delete_directory(_path, recursive)
 
 
 def list_directory(
-    directory: pathlib.Path,
+    path: pathlib.Path,
     is_file: typing.Union[bool, None] = None,
 ) -> collections.abc.Iterator:
     """
-    List contents of directory
+    Helper method to list contents of a directory
 
     Args:
-        directory: Directory path to be iterated
+        path: Directory path to be iterated
         is_file: Return files, directories or both in results
     """
-    assert directory.exists(), f"Invalid directory path"
-    assert directory.is_dir(), f"Invalid directory path"
+    validate_path_exists(path)
+    validate_is_file(path)
 
-    for item in directory.iterdir():
+    for item in path.iterdir():
         if is_file is not None:
             if is_file and item.is_file():
                 yield item
@@ -64,31 +159,6 @@ def list_directory(
                 yield item
         else:
             yield item
-
-
-def validate_file_size(
-    source: pathlib.Path,
-    lower_bound: int,
-    upper_bound: int,
-) -> bool:
-    """
-    Validate file size is between specified ranges
-
-    Args:
-        source: Source file path to be validated
-        lower_bound: Acceptable lower bound range of file size in bytes
-        upper_bound: Acceptable upper bound range of file size in bytes
-    """
-    assert not lower_bound >= upper_bound, (
-        f"Value of lower bound range cannot be greater "
-        f"than or equal to value of upper bound range"
-    )
-    assert source.is_file(), (
-        f"Source path is referring to a directory, please "
-        f"provide a source path that leads to a file"
-    )
-
-    return lower_bound < source.stat().st_size < upper_bound
 
 
 def generate_file_hash(
@@ -102,8 +172,10 @@ def generate_file_hash(
         source: Source file path to generate hash
         algorithM: Hashing algorithm to use. Example: "md5", "sha256"
     """
-    assert source.is_file(), f"Invalid source path"
-    assert algorithm in ["md5", "sha256"], f"Unsupported hash algorithm"
+    validate_is_directory(source)
+
+    if algorithm not in ["md5", "sha256"]:
+        raise Exception(f"Unsupported hash algorithm")
 
     hasher = hashlib.new(algorithm)
 
@@ -112,19 +184,3 @@ def generate_file_hash(
             hasher.update(chunk)
 
     return hasher.hexdigest()
-
-
-def validate_file_hash(
-    _hash: str,
-    source: pathlib.Path,
-    algorithm: str = "md5",
-) -> bool:
-    """
-    Validate hash of a file
-
-    Args:
-        hash: Hash value to validate against
-        source: Source file path to generate hash
-        algorithM: Hashing algorithm to use. Example: "md5", "sha256"
-    """
-    return _hash == generate_file_hash(source, algorithm)
