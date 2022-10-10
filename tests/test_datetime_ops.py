e08e7ba6199b514967d7840a7386d8f70fa8ab44
Jeremy Tan <jeremy.tan@astravirtus.com>
2022-10-10 13:50:02 +0800

    Added new tests

--- tests/test_datetime_ops.py
+++ tests/test_datetime_ops.py
@@ -2,12 +2,22 @@ import datetime
 import unittest.mock
 
 import pytest
-from jt_snippets.datetime_ops import DateTimeFormat, export_datetime, get_weekdays, get_weekends
+from jt_snippets.datetime_ops import (
+    DateTimeFormat,
+    export_datetime,
+    get_weekdays,
+    get_weekends,
+    is_weekday,
+    is_weekend,
+    to_datetime,
+)
 
 # region: Helper methods
 # endregion: Helper methods
 
 # region: Mocked resources
+
+
 # endregion: Mocked resources
 
 # region: PyTest parametrized variables
@@ -59,10 +69,26 @@ get_weekends_happy = [
                 datetime.datetime(2022, 9, 25),
             ]
         ),
-    )
+    ),
+    # (
+    #     dict(ranges=()),
+    #     dict(
+    #         result=[
+    #             datetime.datetime(2022, 7, 2),
+    #             datetime.datetime(2022, 7, 3),
+    #             datetime.datetime(2022, 7, 9),
+    #             datetime.datetime(2022, 7, 10),
+    #             datetime.datetime(2022, 7, 16),
+    #             datetime.datetime(2022, 7, 17),
+    #             datetime.datetime(2022, 7, 23),
+    #             datetime.datetime(2022, 7, 24),
+    #             datetime.datetime(2022, 7, 30),
+    #             datetime.datetime(2022, 7, 31),
+    #         ]
+    #     ),
+    # ),
 ]
 get_weekends_sad = []
-
 
 get_weekdays_happy = [
     (
@@ -98,6 +124,92 @@ get_weekdays_happy = [
 ]
 get_weekends_sad = []
 
+to_datetime_happy = [
+    (
+        dict(timestamp=1651382415),
+        dict(result=datetime.datetime(2022, 5, 1, 13, 20, 15)),
+    ),
+    (
+        dict(timestamp=1651558230.0),
+        dict(result=datetime.datetime(2022, 5, 3, 14, 10, 30)),
+    ),
+    (
+        dict(timestamp=1651841100000),
+        dict(result=datetime.datetime(2022, 5, 6, 20, 45, 00)),
+    ),
+]
+to_datetime_sad = []
+
+
+is_weekday_happy = [
+    (
+        dict(
+            day=datetime.datetime(2022, 6, 27, 14, 20, 15),
+        ),
+        dict(
+            result=True,
+        ),
+    ),
+    (
+        dict(
+            day=datetime.date(2022, 6, 28),
+        ),
+        dict(
+            result=True,
+        ),
+    ),
+]
+is_weekday_sad = [
+    (
+        dict(
+            day=datetime.datetime(2022, 6, 26, 9, 10, 5),
+        ),
+        dict(
+            result=False,
+        ),
+    ),
+    (
+        dict(
+            day=datetime.date(2022, 6, 25),
+        ),
+        dict(
+            result=False,
+        ),
+    ),
+]
+
+is_weekend_happy = [
+    (
+        dict(
+            day=datetime.datetime(2022, 7, 9, 18, 5, 0),
+        ),
+        dict(
+            result=True,
+        ),
+    ),
+    (
+        dict(day=datetime.date(2022, 7, 10)),
+        dict(
+            result=True,
+        ),
+    ),
+]
+is_weekend_sad = [
+    (
+        dict(
+            day=datetime.datetime(2022, 7, 12, 16, 0),
+        ),
+        dict(
+            result=False,
+        ),
+    ),
+    (
+        dict(day=datetime.date(2022, 7, 13)),
+        dict(
+            result=False,
+        ),
+    ),
+]
 
 # endregion: PyTest parametrized variables
 
@@ -124,3 +236,33 @@ class TestDateTimeOps:
         assert len(result) == len(expect["result"])
         for index, item in enumerate(result):
             assert item == expect["result"][index]
+
+    @pytest.mark.parametrize("payload, expect", to_datetime_happy)
+    def test_happy_to_datetime(self, payload, expect):
+        result = to_datetime(**payload)
+
+        assert result == expect["result"]
+
+    @pytest.mark.parametrize("payload, expect", is_weekday_happy)
+    def test_happy_is_weekday(self, payload, expect):
+        result = is_weekday(**payload)
+
+        assert result == expect["result"]
+
+    @pytest.mark.parametrize("payload, expect", is_weekday_sad)
+    def test_sad_is_weekday(self, payload, expect):
+        result = is_weekday(**payload)
+
+        assert result == expect["result"]
+
+    @pytest.mark.parametrize("payload, expect", is_weekend_happy)
+    def test_happy_is_weekend(self, payload, expect):
+        result = is_weekend(**payload)
+
+        assert result == expect["result"]
+
+    @pytest.mark.parametrize("payload, expect", is_weekend_sad)
+    def test_sad_is_weekend(self, payload, expect):
+        result = is_weekend(**payload)
+
+        assert result == expect["result"]
