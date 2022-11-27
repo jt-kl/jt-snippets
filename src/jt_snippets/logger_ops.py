95d1c0f912b1d5b81088350a3f95f18d8ba954ec
Jeremy Tan <jeremy.tan@astravirtus.com>
2022-11-27 12:22:02 +0800

    Parametrize logging level.

--- src/jt_snippets/logger_ops.py
+++ src/jt_snippets/logger_ops.py
@@ -2,10 +2,12 @@ from logging import DEBUG, Formatter, Logger, StreamHandler, getLogger
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 from sys import stdout
+from typing import Optional
 
 
 def create_logger(
-    directory: Path,
+    directory: Optional[Path] = None,
+    level: int = DEBUG,
 ) -> Logger:
     """
     Create custom application logger
@@ -13,24 +15,30 @@ def create_logger(
     Args:
         directory: Storage path for log file
     """
-    directory.mkdir(exist_ok=True)
-
     logger = getLogger(__name__)
-    logger.setLevel(DEBUG)
+    logger.setLevel(level)
 
-    # Log file handler
-    log_file = directory.joinpath("application.log")
     log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
 
-    file_handler = RotatingFileHandler(log_file, maxBytes=10485760, backupCount=10)
-    file_handler.setLevel(DEBUG)
-    file_handler.setFormatter(log_format)
-    logger.addHandler(file_handler)
+    if directory:
+        directory.mkdir(exist_ok=True, parents=True)
 
-    # STDOUT stream handler
+        # Log file handler
+        log_file = directory.joinpath("application.log")
+
+        file_handler = RotatingFileHandler(log_file, maxBytes=10485760, backupCount=10)
+
+        file_handler.setLevel(level)
+        file_handler.setFormatter(log_format)
+
+        logger.addHandler(file_handler)
+
+    # Stream handler
     stream_handler = StreamHandler(stdout)
-    stream_handler.setLevel(DEBUG)
+
+    stream_handler.setLevel(level)
     stream_handler.setFormatter(log_format)
+
     logger.addHandler(stream_handler)
 
     # TODO: Add syslog handler
