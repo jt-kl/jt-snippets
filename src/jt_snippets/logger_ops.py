8d96254a17b9976ed23d2c9706e8dcc8fc5ec252
Jeremy Tan <jeremy.tan@astravirtus.com>
2022-11-29 19:52:53 +0800

    Fixed logging propagating to root logger

--- src/jt_snippets/logger_ops.py
+++ src/jt_snippets/logger_ops.py
@@ -8,39 +8,45 @@ from typing import Optional
 def create_logger(
     name: Optional[str] = __name__,
     directory: Optional[Path] = None,
+    file: str = "application.log",
     level: int = DEBUG,
 ) -> Logger:
     """
     Create custom application logger
 
     Args:
-        directory: Storage path for log file
+        path: Directory path to store log file
+        file: Name of log file
     """
     logger = getLogger(name)
     logger.setLevel(level)
 
     log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
 
-    if directory:
-        directory.mkdir(exist_ok=True, parents=True)
+    if not logger.hasHandlers():
+        # Prevent logging from propagating to the root logger
+        logger.propagate = False
 
-        # Log file handler
-        log_file = directory.joinpath("application.log")
+        # Stream handler
+        stream_handler = StreamHandler(stdout)
 
-        file_handler = RotatingFileHandler(log_file, maxBytes=10485760, backupCount=10)
+        stream_handler.setLevel(level)
+        stream_handler.setFormatter(log_format)
 
-        file_handler.setLevel(level)
-        file_handler.setFormatter(log_format)
+        logger.addHandler(stream_handler)
 
-        logger.addHandler(file_handler)
+        if directory:
+            directory.mkdir(exist_ok=True, parents=True)
 
-    # Stream handler
-    # stream_handler = StreamHandler(stdout)
+            # Log file handler
+            log_file = directory.joinpath(file)
 
-    # stream_handler.setLevel(level)
-    # stream_handler.setFormatter(log_format)
+            file_handler = RotatingFileHandler(log_file, maxBytes=10485760, backupCount=10)
 
-    # logger.addHandler(stream_handler)
+            file_handler.setLevel(level)
+            file_handler.setFormatter(log_format)
+
+            logger.addHandler(file_handler)
 
     # TODO: Add syslog handler
 
