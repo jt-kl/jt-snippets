87a9f8df3d3af1641a49f4e5d03ecac8eef31544
Jeremy Tan <jeremy.tan@astravirtus.com>
2023-04-12 08:48:11 +0800

    Revise logger helpers

--- src/jt_snippets/logger_ops.py
+++ src/jt_snippets/logger_ops.py
@@ -1,54 +1,37 @@
-from logging import DEBUG, Formatter, Logger, StreamHandler, getLogger
+from logging import DEBUG, Formatter, Handler, Logger, StreamHandler, getLogger
 from logging.handlers import RotatingFileHandler, SysLogHandler
 from pathlib import Path
 from sys import stdout
-from typing import Any, Optional, TextIO
-
-
-def add_rotating_file_handler(
-    logger,
-    directory: Path,
-    file: str = "application.log",
-    level: int = DEBUG,
-):
-    """
-    Add a RotatingFileHandler handler to logger
-
-    Args:
-        logger: Instance of logger
-        directory: Directory path to store log file
-        file: Name of log file
-        level: Logging level
-    """
-    log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
-
-    rotating_file_handler = RotatingFileHandler(
-        directory.joinpath(file),
-        maxBytes=10485760,
-        backupCount=10,
-    )
-    rotating_file_handler.setLevel(level)
-    rotating_file_handler.setFormatter(log_format)
-
-    logger.addHandler(rotating_file_handler)
+from typing import Optional, TextIO
 
 
 def create_console_stream_handler(
     stream: TextIO = stdout,
+    level: int = DEBUG,
+    format: Optional[str] = None,
 ) -> StreamHandler:  # type: ignore
     """
     Creates a console stream handler
 
     Args:
         stream: Logging output stream
+        level: Logging level
+        format: Log output format string
     """
     console_stream_handler = StreamHandler(stream)
+    console_stream_handler.setLevel(level)
+
+    if format:
+        log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
+        console_stream_handler.setFormatter(log_format)
 
     return console_stream_handler
 
 
 def create_syslog_handler(
     address: tuple[str, int] = ("localhost", 514),
+    level: int = DEBUG,
+    format: Optional[str] = None,
     **kwargs,
 ) -> SysLogHandler:
     """
@@ -56,11 +39,18 @@ def create_syslog_handler(
 
     Args:
         address: Hostname and port number of SysLog host
+        level: Logging level
+        format: Log output format string
     """
     syslog_handler = SysLogHandler(
         address,
         **kwargs,
     )
+    syslog_handler.setLevel(level)
+
+    if format:
+        log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
+        syslog_handler.setFormatter(log_format)
 
     return syslog_handler
 
@@ -68,6 +58,8 @@ def create_syslog_handler(
 def create_rotating_file_handler(
     directory: Path,
     filename: str = "application.log",
+    level: int = DEBUG,
+    format: Optional[str] = None,
     maximum_file_size: int = 10485760,
     maximum_file_count: int = 10,
     **kwargs,
@@ -78,6 +70,8 @@ def create_rotating_file_handler(
     Args:
         directory: Directory path to store log file
         filename: Desired log file name
+        level: Logging level
+        format: Log output format string
         maximum_file_size: Maximum file size before rolling over
         maximum_file_count: Maximum file count before rolling over
     """
@@ -87,6 +81,11 @@ def create_rotating_file_handler(
         backupCount=maximum_file_count,
         **kwargs,
     )
+    rotating_file_handler.setLevel(level)
+
+    if format:
+        log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
+        rotating_file_handler.setFormatter(log_format)
 
     return rotating_file_handler
 
@@ -94,7 +93,8 @@ def create_rotating_file_handler(
 def create_logger(
     name: Optional[str] = None,
     level: int = DEBUG,
-    handlers: list[Any] = [],
+    format: Optional[str] = None,
+    handlers: list[Handler] = [],
 ) -> Logger:
     """
     Create custom application logger
@@ -102,29 +102,21 @@ def create_logger(
     Args:
         name: Name of logger
         level: Logging level
+        format: Log output format string
         handlers: Collection of handlers to add to logger
     """
-    log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
-
     logger = getLogger(name)
     logger.setLevel(level)
 
     # Prevent logging from propagating to the root logger
     logger.propagate = False
 
-    if not logger.hasHandlers():
-        # Console stream handler
-        console_stream_handler = StreamHandler(stdout)
-        console_stream_handler.setLevel(level)
-        console_stream_handler.setFormatter(log_format)
-
-        logger.addHandler(console_stream_handler)
-
     for handler in handlers:
-        handler.setLevel(level)
-        handler.setFormatter(log_format)
-        logger.addHandler(handler)
+        # Override log output format
+        if format:
+            log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
+            handler.setFormatter(log_format)
 
-    # TODO: Add syslog handler
+        logger.addHandler(handler)
 
     return logger
