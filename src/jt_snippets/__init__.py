a95ed73b444d629030ef7e3b26951ce46c192c3c
Jeremy Tan <jeremy.tan@astravirtus.com>
2023-12-04 13:14:01 +0800

    Updated version number

--- src/jt_snippets/__init__.py
+++ src/jt_snippets/__init__.py
@@ -1,15 +1,1 @@
-from logging import INFO, Formatter, StreamHandler, getLogger
-from sys import stdout
-
-VERSION = "1.0.8+build.1"
-
-
-log_format = Formatter("%(asctime)s|%(levelname)-8s|%(module)s:%(funcName)s:%(lineno)d - %(message)s")
-
-stream_handler = StreamHandler(stdout)
-stream_handler.setLevel(INFO)
-stream_handler.setFormatter(log_format)
-
-logger = getLogger()
-logger.setLevel(INFO)
-logger.addHandler(stream_handler)
+VERSION = "1.0.9"
